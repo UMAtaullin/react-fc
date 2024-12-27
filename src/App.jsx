f52cbb4d92603da69efcfb29c5f608b5b81ccf99
UMAtaullin <ataullinural@gmail.com>
2024-12-27 19:30:17 +0500

    binding state

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import './App.css'
 function App() {
 
   const [likes, setLikes] = useState(5)
+  const [value, setValues] = useState('some text')
   
   function plus() {
     setLikes(likes + 1)
@@ -16,7 +17,13 @@ function App() {
 
   return (
     <>
-      <h1>{likes}</h1>
+      <h2>{value}</h2>
+      <h2>{likes}</h2>
+      <input 
+        type="text" 
+        value={value}
+        onChange={e => setValues(e.target.value)} 
+        />
       <button onClick={plus}>Increment</button>
       <button onClick={minus}>Decrement</button>
     </>
